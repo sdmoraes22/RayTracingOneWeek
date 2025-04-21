102b87e1da14c4f6ff961e3966d9f3b3c4ad357b
Cristiano <cmoraes.ti@gmail.com>
2025-04-20 23:55:30 -0300

    Limmiting difuse ray bouncing

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ public:
     double aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable &world)
     {
@@ -26,7 +27,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; sample++)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth ,world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -89,14 +90,17 @@ private:
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray &r, const hittable &world) const
+    color ray_color(const ray &r, int depth, const hittable &world) const
     {
+        if(depth <= 0)
+            return color(0, 0, 0);
+
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec))
         {
             vec3 direction = random_on_emisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth -1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
